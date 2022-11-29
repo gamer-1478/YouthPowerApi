0fcd25a8794a058c3d4970720a6bebeeddef0f43
Aayush Garg <aayushgarg2006@gmail.com>
2022-11-29 15:39:17 +0530

    update

--- server.js
+++ server.js
@@ -16,6 +16,8 @@ app.get('/', (req, res) => {
 app.get('/images', (req, res) => {
     const images = []
     const images1 = fs.readdirSync('./images1');
+    const images2 = fs.readdirSync('./images2');
+    const images3 = fs.readdirSync('./images3');
     //make array of absolute paths
     images1.forEach(image => {
         images.push({ 
@@ -23,6 +25,21 @@ app.get('/images', (req, res) => {
         original: `https://yp-website-api.herokuapp.com/image/images1/${image}`,
             tags: [{ value: "Activity 1", title: "Activity 1" }], })
     });
+    images2.forEach(image => {
+        images.push({
+            src: `https://yp-website-api.herokuapp.com/image/images2/${image}`,
+            original: `https://yp-website-api.herokuapp.com/image/images2/${image}`,
+            tags: [{ value: "Activity 2", title: "Activity 2" }],
+        })
+    });
+    images3.forEach(image => {
+        images.push({
+            src: `https://yp-website-api.herokuapp.com/image/images3/${image}`,
+            original: `https://yp-website-api.herokuapp.com/image/images3/${image}`,
+            tags: [{ value: "Activity 3", title: "Activity 3" }],
+        })
+    });
+
     res.send(images);
 })
 
