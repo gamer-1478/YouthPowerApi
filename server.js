3c8011dd9334cb0af0b13425379dce2b46be7dfd
Aayush Garg <aayushgarg2006@gmail.com>
2023-01-28 00:43:06 +0530

    images

--- server.js
+++ server.js
@@ -18,6 +18,11 @@ app.get('/images', (req, res) => {
     const images1 = fs.readdirSync('./images1');
     const images2 = fs.readdirSync('./images2');
     const images3 = fs.readdirSync('./images3');
+    const images4 = fs.readdirSync('./images4');
+    const images5 = fs.readdirSync('./images5');
+    const images6 = fs.readdirSync('./images6');
+    const images7 = fs.readdirSync('./images7');
+
     //make array of absolute paths
     images1.forEach(image => {
         images.push({ 
@@ -38,6 +43,33 @@ app.get('/images', (req, res) => {
             original: `https://yp-website-api.herokuapp.com/image/images3/${image}`,
             tags: [{ value: "Activity 3", title: "Activity 3" }],
         })
+    });
+    images4.forEach(image => {
+        images.push({
+            src: `https://yp-website-api.herokuapp.com/image/images1/${image}`,
+            original: `https://yp-website-api.herokuapp.com/image/images1/${image}`,
+            tags: [{ value: "Activity 4", title: "Activity 4" }],
+        })
+    });
+    images5.forEach(image => {
+        images.push({
+            src: `https://yp-website-api.herokuapp.com/image/images2/${image}`,
+            original: `https://yp-website-api.herokuapp.com/image/images2/${image}`,
+            tags: [{ value: "Activity 5", title: "Activity 5" }],
+        })
+    });
+    images6.forEach(image => {
+        images.push({
+            src: `https://yp-website-api.herokuapp.com/image/images3/${image}`,
+            original: `https://yp-website-api.herokuapp.com/image/images3/${image}`,
+            tags: [{ value: "Activity 6", title: "Activity 6" }],
+        })
+    });
+    images7.forEach(image => {
+        images.push({ 
+            src: `https://yp-website-api.herokuapp.com/image/images1/${image}`, 
+        original: `https://yp-website-api.herokuapp.com/image/images1/${image}`,
+            tags: [{ value: "Activity 7", title: "Activity 7" }], })
     });
 
     res.send(images);
